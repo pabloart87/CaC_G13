0990f8909769e8ae097369e58fddd9d7ee111fd0
pabloart87 <pablo_ariel_toledo@hotmail.com>
2023-05-04 22:24:43 -0300

    modificacion templates

--- proj_cac/cac_2023/views.py
+++ proj_cac/cac_2023/views.py
@@ -6,10 +6,12 @@ def index(request):
     return HttpResponse("hola mundo django 2023!!!")
 
 def ingreso_datos(request):
-    return HttpResponse("INGRESO DE DATOS")
+    context = {}
+    return render(request, "cac_2023/ingreso_datos.html", context)
 
 def eliminar_datos(request):
-    return HttpResponse("ELIMINAR DATOS")
+    context = {}
+    return render(request, "cac_2023/eliminar_datos.html", context)
 
 def calcular_edad(request, edad, agno):
     periodo = agno - 2023
@@ -18,10 +20,58 @@ def calcular_edad(request, edad, agno):
     return HttpResponse(documento) 
 
 def index2(request):
-    return render(request, "index2.html")
+    
+    alumno_ficticio = {
+        "name" : "Bianca",
+        "last_name" : "Concatti",
+        "age" : 26,
+        "valid" : False
+    }
+    
+    listado_alumnos = [ 
+    {
+        "name" : "Bianca",
+        "last_name" : "Concatti",
+        "age" : 26,
+        "valid" : False
+    },
+    {
+        "name" : "Martin",
+        "last_name" : "El martu",
+        "age" : 26,
+        "valid" : False
+    },
+    {
+        "name" : "Cheba",
+        "last_name" : "El pancho",
+        "age" : 26,
+        "valid" : False
+    },
+    {
+        "name" : "La",
+        "last_name" : "Lola",
+        "age" : 26,
+        "valid" : False
+    },
+    ]
+    
+    context = {
+        "name": "Pablo",
+        "last_name": "Toledo",
+        "alumn": alumno_ficticio,
+        "listado_alumnos": listado_alumnos
+        }
+    
+    return render(request, "cac_2023/index2.html", context)
 
 def consulta(request):
-    return render(request, "consulta.html")
+    
+    context = {}
+    
+    return render(request, "cac_2023/consulta.html", context)
 
 def inicio(request):
-    return render(request, "inicio.html")
+    
+    context = {}
+    
+    return render(request, "cac_2023/inicio.html", context )
